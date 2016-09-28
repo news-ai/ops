adfd2eaf6006924f86cad073d5df1cac881dbaa6
Abhi Agarwal <me@abhiagarwal.com>
2016-09-27 20:54:25 -0400

    ListId sometimes just aren't there - filters don't help

--- datastore/remove_contacts_without_list_id.py
+++ datastore/remove_contacts_without_list_id.py
@@ -4,10 +4,13 @@ client = datastore.Client('newsai-1166')
 
 
 query = client.query(kind='Contact')
-query.add_filter('ListId', '=', 0)
-query.add_filter('IsMasterContact', '=', False)
-contacts = list(query.fetch())
 
-for contact in contacts:
-    print contact.key
-    client.delete(contact.key)
+# contacts = list(query.fetch())
+
+# for contact in contacts:
+#     print contact.key
+#     client.delete(contact.key)
+
+for result in query.fetch():
+    if 'ListId' not in result:
+        client.delete(result.key)
