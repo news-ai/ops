ec375d3cc72d72a93399dc5b1e51d7b828dd96b2
Abhi Agarwal <me@abhiagarwal.com>
2016-09-27 21:51:51 -0400

    Finishing query

--- datastore/remove_contacts_without_list_id.py
+++ datastore/remove_contacts_without_list_id.py
@@ -2,7 +2,6 @@ from gcloud import datastore
 
 client = datastore.Client('newsai-1166')
 
-
 query = client.query(kind='Contact')
 
 # contacts = list(query.fetch())
@@ -11,6 +10,11 @@ query = client.query(kind='Contact')
 #     print contact.key
 #     client.delete(contact.key)
 
+count = 0
+
 for result in query.fetch():
+    count += 1
+    print count
     if 'ListId' not in result:
+        print result
         client.delete(result.key)
