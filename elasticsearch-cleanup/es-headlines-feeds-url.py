5d1f34a517eadc39a576dd83375c8f3da4be7ae7
Abhi Agarwal <me@abhiagarwal.com>
2016-10-14 20:28:29 -0400

    Working on #14

---
+++ elasticsearch-cleanup/es-headlines-feeds-url.py
@@ -0,0 +1,25 @@
+# Stdlib imports
+import os
+
+# Third-party app imports
+import certifi
+from elasticsearch import Elasticsearch, helpers
+
+# Elasticsearch
+ELASTICSEARCH_USER = os.environ['NEWSAI_ELASTICSEARCH_USER']
+ELASTICSEARCH_PASSWORD = os.environ['NEWSAI_ELASTICSEARCH_PASSWORD']
+
+# Elasticsearch setup
+es = Elasticsearch(
+    ['https://search1.newsai.org'],
+    http_auth=(ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD),
+    port=443,
+    use_ssl=True,
+    verify_certs=True,
+    ca_certs=certifi.where(),
+)
+
+results = es.search(index='headlines', doc_type='headline', size=5000)
+
+for result in results['hits']['hits']:
+    print result
