9db8d88ffa836fc2c2e7afa47b59393ec949cc74
Abhi Agarwal <me@abhiagarwal.com>
2017-03-04 19:21:27 -0500

    sync internal emails

---
+++ datastore/datastore_internal_contacts.py
@@ -0,0 +1,39 @@
+# Stdlib imports
+import urllib3
+import os
+import json
+from datetime import datetime, timedelta
+
+# Third-party app imports
+import requests
+import certifi
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from elasticsearch import Elasticsearch, helpers
+
+# Elasticsearch
+ELASTICSEARCH_USER = os.environ['NEWSAI_ELASTICSEARCH_USER']
+ELASTICSEARCH_PASSWORD = os.environ['NEWSAI_ELASTICSEARCH_PASSWORD']
+
+# Removing requests warning
+urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Elasticsearch setup
+es = Elasticsearch(
+    ['https://search1.newsai.org'],
+    http_auth=(ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD),
+    port=443,
+    use_ssl=True,
+    verify_certs=True,
+    ca_certs=certifi.where(),
+)
+
+
+def process_contacts(contacts):
+    for contact in contacts['hits']['hits']:
+        email = contact['_source']['data']['Email']
+        if email:
+            print email
+
+contacts = es.search(index="contacts", body={}, size=100, from_=0)
+process_contacts(contacts)
