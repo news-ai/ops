d8034e2210633aa47d6a7679b21510c2454750ce
Abhi Agarwal <me@abhiagarwal.com>
2017-04-03 17:51:41 -0400

    initial script to ts of emails

---
+++ datastore/email_sync.py
@@ -0,0 +1,93 @@
+# Stdlib imports
+import urllib3
+import os
+import re
+import json
+from datetime import datetime, timedelta
+
+# Third-party app imports
+import requests
+import certifi
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from elasticsearch import Elasticsearch, helpers
+from gcloud import datastore
+
+# Elasticsearch
+ELASTICSEARCH_USER = os.environ['NEWSAI_ELASTICSEARCH_USER']
+ELASTICSEARCH_PASSWORD = os.environ['NEWSAI_ELASTICSEARCH_PASSWORD']
+
+# Setup datastore connection for Google Cloud
+client = datastore.Client('newsai-1166')
+
+# Removing requests warning
+urllib3.disable_warnings()
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# Elasticsearch setup
+es = Elasticsearch(
+    ['https://search1.newsai.org'],
+    http_auth=(ELASTICSEARCH_USER, ELASTICSEARCH_PASSWORD),
+    port=443,
+    use_ssl=True,
+    verify_certs=True,
+    ca_certs=certifi.where(),
+)
+
+
+def get_email_logs():
+    page = es.search(
+        index='emails',
+        doc_type='email',
+        scroll='2m',
+        search_type='scan',
+        size=1000,
+        body={}
+    )
+
+    user_to_emails = {}
+
+    sid = page['_scroll_id']
+    scroll_size = page['hits']['total']
+
+    while (scroll_size > 0):
+        page = es.scroll(scroll_id=sid, scroll='2m')
+        sid = page['_scroll_id']
+        scroll_size = len(page['hits']['hits'])
+        print "scroll size: " + str(scroll_size)
+
+        for email in page['hits']['hits']:
+            email_id = email['_source']['data']['CreatedBy']
+
+            if email_id not in user_to_emails:
+                user_to_emails[email_id] = []
+
+            user_to_emails[email_id].append(email['_source']['data'])
+
+    return user_to_emails
+
+def user_emails_to_timeseries(user_emails):
+    date_to_email = {}
+    for email in user_emails:
+        datetime_object = email['Created'][0:10]
+
+        if datetime_object not in date_to_email:
+            date_to_email[datetime_object] = {}
+
+        # Append Opens
+        if 'Opens' not in date_to_email[datetime_object]:
+            date_to_email[datetime_object]['Opens'] = 0
+        date_to_email[datetime_object]['Opens'] += email['Opened']
+
+        # Append Clicks
+        if 'Clicks' not in date_to_email[datetime_object]:
+            date_to_email[datetime_object]['Clicks'] = 0
+        date_to_email[datetime_object]['Clicks'] += email['Clicked']
+
+        # Append Opens
+        if 'Amount' not in date_to_email[datetime_object]:
+            date_to_email[datetime_object]['Amount'] = 0
+        date_to_email[datetime_object]['Amount'] += 1
+
+    print date_to_email
+
+email_dictionary = get_email_logs()
